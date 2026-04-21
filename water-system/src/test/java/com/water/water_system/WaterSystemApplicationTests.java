057bf94bc1814bc7487d8fcc7cae19461e0e2dcf
samwelmagaiwa <samwelmagaiwa229@gmail.com>
2026-04-22 00:34:31 +0300

    first commit

---
+++ water-system/src/test/java/com/water/water_system/WaterSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.water.water_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
